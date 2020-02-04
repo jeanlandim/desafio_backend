a224f7fdae8342dac19f15a9f57ebd28e8c1c899
Jean Landim <jewanbb@gmail.com>
2020-02-04 11:50:25 -0300

    Finalizando o sistema de autenticação

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
         path('api-textos/',ApiList.as_view(),name='api-textos'),
-        path('api-textos/<pk>',ApiListItems.as_view(),name='api-textos')
+        path('api-textos/<pk>',ApiListItems.as_view(),name='api-textos'),
 ]
 
